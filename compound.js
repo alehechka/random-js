a79e8631a5d1b2cd0f67796e1566f3df5d80d5d3
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2020-11-05 20:43:09 -0600

    Simple Compound

---
+++ compound.js
@@ -0,0 +1,30 @@
+const range = (size, startAt = 0) => {
+	return [...Array(size).keys()].map((i) => i + startAt);
+};
+
+const compoundOneYear = (prevAmount, interestRate, rate = 1) => {
+	return range(rate).reduce((amount) => {
+		return amount + (amount * interestRate) / rate;
+	}, prevAmount);
+};
+
+const compoundYears = (prevAmount, interestRate, years, rate = 1) => {
+	return range(years).reduce((amount) => {
+		return compoundOneYear(amount, interestRate, rate);
+	}, prevAmount);
+};
+
+(() => {
+	const f = process.argv[2] || 'simple';
+	let response = 'no calculations made';
+	switch (f) {
+		case 'simple':
+			response = compoundYears(
+				parseFloat(process.argv[3]) || 100, // starting amount
+				parseFloat(process.argv[4]) || 0.05, // interest rate
+				parseFloat(process.argv[5]) || 10, // number of years
+				parseFloat(process.argv[6]) || 12 // rate of acc
+			);
+	}
+	console.log(parseFloat(response) ? response.toFixed(2) : response);
+})();
